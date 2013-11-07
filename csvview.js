2e823ae21ccc06e3f3a5309e68beab5bc4eb1974
Antony Courtney <antony.courtney@gmail.com>
2013-11-06 16:30:07 -0800

    move all csvview js into one file

--- csvview.js
+++ csvview.js
@@ -2,9 +2,173 @@
   // Slick.Grid
   $.extend(true, window, {
     CSVView: {
-      View: CSVViewer
+      View: CSVViewer,
+      Data: {
+        RemoteModel: RemoteModel 
+      }
     }
   });
+
+  /***
+   * A sample AJAX data store implementation.
+   * Modified by ANT to load CSV data from a Python script served via cherrypy
+   */
+  function RemoteModel( tn ) {
+    // private
+    var PAGESIZE = 50;
+    var data = {length: 0};
+    var searchstr = "";
+    var sortcol = null;
+    var sortdir = 1;
+    var h_request = null;
+    var req = null; // ajax request
+    var table_name = tn;
+
+    // events
+    var onDataLoading = new Slick.Event();
+    var onDataLoaded = new Slick.Event();
+
+
+    function init() {
+    }
+
+
+    function isDataLoaded(from, to) {
+      for (var i = from; i <= to; i++) {
+        if (data[i] == undefined || data[i] == null) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+
+    function clear() {
+      for (var key in data) {
+        delete data[key];
+      }
+      data.length = 0;
+    }
+
+
+    function ensureData(from, to) {
+      // console.log( "ensureData( ", from, ", ", to, " ) ")
+      if (req) {
+        req.abort();
+        for (var i = req.fromPage; i <= req.toPage; i++)
+          data[i * PAGESIZE] = undefined;
+      }
+
+      if (from < 0) {
+        from = 0;
+      }
+
+      if (data.length > 0) {
+        to = Math.min(to, data.length - 1);
+      }
+
+      var fromPage = Math.floor(from / PAGESIZE);
+      var toPage = Math.floor(to / PAGESIZE);
+
+      while (data[fromPage * PAGESIZE] !== undefined && fromPage < toPage)
+        fromPage++;
+
+      while (data[toPage * PAGESIZE] !== undefined && fromPage < toPage)
+        toPage--;
+
+      // console.log( " fromPage: ", fromPage, ", toPage: ", toPage );
+
+      if (fromPage > toPage || ((fromPage == toPage) && data[fromPage * PAGESIZE] !== undefined)) {
+        // TODO:  look-ahead
+        onDataLoaded.notify({from: from, to: to});
+        return;
+      }
+
+      var url = "tables/" + tn
+      if (h_request != null) {
+        clearTimeout(h_request);
+      }
+
+      // console.log( "about to send request for [ ", from, ", ", to,  " ) ");
+
+      h_request = setTimeout(function () {
+        for (var i = fromPage; i <= toPage; i++)
+          data[i * PAGESIZE] = null; // null indicates a 'requested but not available yet'
+
+        onDataLoading.notify({from: from, to: to});
+
+        reqParams = { startRow: fromPage * PAGESIZE, rowLimit: (((toPage - fromPage) * PAGESIZE) + PAGESIZE)};
+
+        if (sortcol != null )
+          reqParams.sortby = sortcol + ((sortdir > 0) ? "+asc" : "+desc");
+
+        req = $.get( url, reqParams, onSuccess ); 
+
+        req.fromPage = fromPage;
+        req.toPage = toPage;
+      }, 50);
+    }
+
+
+    function onError(fromPage, toPage) {
+      alert("error loading pages " + fromPage + " to " + toPage);
+    }
+
+    function onSuccess(resp) {
+      console.log( "Got ", resp.results.length, " rows ( of ", resp.totalRowCount, " ) from server, startRow: ", resp.request.startRow );
+      // console.log( "onSuccess!" );
+      // console.log( resp );
+      
+      var from = resp.request.startRow, to = from + resp.results.length;
+      data.length = parseInt( resp.totalRowCount );  
+
+      for (var i = 0; i < resp.results.length; i++) {
+        var item = resp.results[i];
+
+        data[from + i] = item;
+        data[from + i].index = from + i;
+      }
+
+      req = null;
+
+      onDataLoaded.notify({from: from, to: to});
+    }
+
+
+    function reloadData(from, to) {
+      for (var i = from; i <= to; i++)
+        delete data[i];
+
+      ensureData(from, to);
+    }
+
+
+    function setSort(column, dir) {
+      sortcol = column;
+      sortdir = dir;
+      clear();
+    }
+
+    init();
+
+    return {
+      // properties
+      "data": data,
+
+      // methods
+      "clear": clear,
+      "isDataLoaded": isDataLoaded,
+      "ensureData": ensureData,
+      "reloadData": reloadData,
+      "setSort": setSort,
+
+      // events
+      "onDataLoading": onDataLoading,
+      "onDataLoaded": onDataLoaded
+    };
+  }
+
 
   function CSVViewer( container, tableName )
   {
