f1d6b7f5ae1af98fed799d4cc166bcf7c061d5a4
Antony Courtney <antony.courtney@gmail.com>
2014-01-13 16:16:15 -0800

    first trivial aggTreeTest passing

--- tests/aggTreeTest.js
+++ tests/aggTreeTest.js
@@ -1,28 +1,49 @@
 
+function columnSum( tableData, columnId ) {
+  var sum = 0;
+
+  var colIndex = tableData.schema.columnIndex( columnId );
+  for ( var i=0; i < tableData.rowData.length; i++ ) {
+    sum += tableData.rowData[i][colIndex];
+  };
+  return sum;
+}
 var rt = relTab.local();
 
-asyncTest("basic aggTree functionality", function() {
+asyncTest("basic aggTree functionality", 3, function() {
   var q0 = relTab.query().table( "bart-comp-all" ).project( [ "Job", "Title", "Union", "Name", "Base", "TCOE" ]);
 
-  var p1 = aggTree.vpivot( rt, q0, ["Job", "Title" ] );
+  var p0 = aggTree.vpivot( rt, q0, ["Job", "Title" ] );
 
   console.log( "called vpiot, got promise result..." );
 
-  p1.then( function( tree0 ) {
+  p0.then( function( tree0 ) {
     console.log( "vpivot initial promise resolved..." );
 
     var q1 = tree0.applyPath( [] );
     console.log( "query for level 0 of tree: ", q1.toString() );
     console.log( q1 );
 
-    var p2 = rt.evalQuery( q1 );
-    p2.then( onQ1Result );
+    var p1 = rt.evalQuery( q1 );
+    p1.then( onQ1Result );
+
   } );
+
 
 } );
 
 function onQ1Result( res ) {
   console.log( "OnQ1Result: ", res );
-  ok();
+  console.log( "schema cols: ", res.schema.columns );
+  console.log( "rowData length: ", res.rowData.length );
+
+  var expCols = ["Job", "Job", "Title", "Union", "Name", "Base", "TCOE"];
+
+  deepEqual( res.schema.columns, expCols, "Q1 schema columns" );
+  deepEqual( res.rowData.length, 19, "Q1 rowData length" );
+
+  var actSum = columnSum( res, "TCOE" );
+
+  deepEqual( actSum, 349816190, "Q1 rowData sum(TCOE)" );
   start();
 };
